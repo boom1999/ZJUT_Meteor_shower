f7434c8e697883a6b665dd2a36d1934bc45a3207
boom1999 <45328326+boom1999@users.noreply.github.com>
2019-05-09 20:43:38 +0800

    修改显示inline，不用再函数外又加一个;

--- Shape.h
+++ Shape.h
@@ -6,7 +6,8 @@
 #include<stdlib.h>
 #include<iostream>
 #include<cmath>
-using namespace std;
+//using namespace std;
+using std::cout; using std::endl;
 
 class Shape                                                  //抽象类shape
 {
@@ -18,12 +19,12 @@ public:
     void setmembers (double X, double Y, double R);          //声明同时修改（sh_X,sh_Y）以及R的函数
     virtual void InitShape (double x) = 0;	                 //（纯虚函数）初始化形状并画图
     virtual void MoveShape () = 0;                           //（纯虚函数）计算移动后的坐标并画图
-    double gets_X () { return sh_X; };                       //返回对象的sh_X
-    double gets_Y () { return sh_Y; };                       //返回对象的sh_Y
-    double gets_R () { return sh_R; };                       //返回对象的sh_R
-    void sets_X (double x) { sh_X = x; };                    //修改对象的sh_X
-    void sets_Y (double y) { sh_Y = y; };                    //修改对象的sh_Y
-    void sets_R (double r) { sh_R = r; };                    //修改对象的sh_R
+    double gets_X () { return sh_X; }                        //返回对象的sh_X
+    double gets_Y () { return sh_Y; }                        //返回对象的sh_Y
+    double gets_R () { return sh_R; }                        //返回对象的sh_R
+    void sets_X (double x) { sh_X = x; }                     //修改对象的sh_X
+    void sets_Y (double y) { sh_Y = y; }                     //修改对象的sh_Y
+    void sets_R (double r) { sh_R = r; }                     //修改对象的sh_R
     Shape ();                                                //构造函数将形状的初始（sh_X,sh_Y）设置为（0，0）
     virtual ~Shape ();                                       //虚析构函数以满足使用指针的要求
 };
